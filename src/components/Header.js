6b4539cb3f259f8f05769e2cee4c334d3a22e6f4
Klaot <klaot@yandex.ru>
2022-01-07 22:28:14 +0300

    First commit

---
+++ src/components/Header.js
@@ -0,0 +1,63 @@
+import React from "react";
+import reactDom from "react-dom";
+import './Header.css';
+import Logo from '../img/header/logo.png';
+import ComponyInstagram from '../img/header/instagram.png';
+import ComponyTelegram from '../img/header/telegram.png';
+import ComponyVk from '../img/header/vk-2.png';
+import ComponyWhatsapp from '../img/header/whatsapp.png';
+import ClockHeader from '../img/header/clock-2.png';
+import TelephoneHeader from '../img/header/telephone.png';
+
+
+
+ function Header() {
+    return (
+       <div> 
+            <div className="contactInfo container">
+                <div className="logo">
+                    <a href="#">
+                        <img src={Logo} alt={'logo'}/>
+                    </a>
+                    <div>
+                        <h4>Moscovaqual</h4>
+                        <p>Ремонт бытовой техники</p>
+                    </div>
+                </div>
+                <div className="social">
+                    <a href="#">
+                        <img src={ComponyInstagram} alt={'ComponyInstagram'}/>
+                    </a>
+                    <a href="#">
+                        <img src={ComponyTelegram} alt={'ComponyTelegram'}/>
+                    </a>
+                    <a href="#">
+                        <img src={ComponyVk} alt={'ComponyVk'}/>
+                    </a>
+                    <a href="#">
+                        <img src={ComponyWhatsapp} alt={'ComponyWhatsapp'}/>
+                    </a>
+                </div>
+                <div className="timetable">
+                    <div className="timetableClock">
+                        <img src={ClockHeader} alt={'ClockHeader'}/>
+                        <p>Режим работы</p>
+                    </div>
+                    <div>
+                        <p>ПН-ПТ с 07:00 до 24:00</p>
+                        <p>Сб, ВС: с 07:00 до 24:00</p>
+                    </div>
+                </div>
+            <div className="callInfo">
+                <div className="telephone">
+                    <img src={TelephoneHeader} alt={'TelephoneHeader'}/>
+                    <a href="#">+7(495) 777 00 00</a>    
+                </div>
+                <button className="callBtn">Заказать звонок</button>    
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export default Header;
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import reactDom from "react-dom";
+import  render  from "react-dom";
+import  Header  from "../components/Header";
+import './Home.css';
+
+function Home() {
+    return (
+        <div>
+            <Header />
+        </div>
+    );
+}
+
+export default Home;
