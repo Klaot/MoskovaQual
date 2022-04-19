bfe44be43682d8d30e694ffded80f0e7ce5bce0d
Klaot <klaot@yandex.ru>
2022-04-19 23:12:04 +0300

    Переписал вызов модального окна на React Hook

--- src/components/ServicesAndPrices/ModalShowBtn.js
+++ src/components/ServicesAndPrices/ModalShowBtn.js
@@ -1,38 +1,25 @@
 import React from 'react';
+import { useState } from 'react';
 import './ModalShowBtn.css';
 
 
+const ModalShowBtn = () => {
 
+   const [open, setOpen] = useState(false);
 
-class ModalShowBtn extends React.Component {
-
-    constructor () {
-        super()
-        this.state = {
-            open: false //Делаем стэйт и записываем в него объект с ключем и свойством open false
-        }
+   const showModal = () => {
+       setOpen(!open)
     }
 
-    showModal = () => {
-        this.setState(({open}) => ({ // Через метод сэт стэйт и функцию меняем состояние компонента при клике на кнопку
-            open: !open              // с помощью этой функции мы меняем свойство на true
-        }))
+   const closeModal = () => {
+        setOpen(!open)
     }
 
-    closeModal = () => {
-        this.setState(({open}) => ({
-            open: !open
-        }))
-    }
-
-    
-    render() {
-
-        const {open} = this.state; // проверяем если свойство open === true то меняем содержимое переменной с классом 
-        let modalClass = "modal"; // и тем самым добавляем или убераем класс show который показывает модальное окно 
+    let modalClass = "modal";
         if (open) {
             modalClass += " show"
-        }
+        }     
+    
 
     return (
         <div>
@@ -40,7 +27,7 @@ class ModalShowBtn extends React.Component {
               <div className="modal__dialog">
                 <div className="modal__content">
                     <form action="#">
-                        <div className="modal__close" onClick={this.closeModal}>&times;</div>
+                        <div className="modal__close" onClick={closeModal}>&times;</div>
                        <div className="modal__title"><p>Мы свяжемся с вами как можно быстрее!</p></div>
                          <input required placeholder="Ваше имя" name="name" type="text" className="modal__input"/>
                          <input required placeholder="Ваш номер телефона" name="phone" type="phone" className="modal__input"/>
@@ -49,11 +36,9 @@ class ModalShowBtn extends React.Component {
                 </div>
             </div>
          </div>
-            <button onClick={this.showModal} className="ModalShowBtn">Заказать ремонт</button>
+            <button onClick={showModal} className="ModalShowBtn">Заказать ремонт</button>
         </div>
-    
     )
-  }
 }
 
 export default ModalShowBtn;
