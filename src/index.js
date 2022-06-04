38e3b97cb1cd67f80b09cb19562f90e510c4cdc5
Klaot <klaot@yandex.ru>
2022-06-04 08:07:44 +0300

    Обновил пакеты

--- src/index.js
+++ src/index.js
@@ -9,9 +9,11 @@ import reportWebVitals from './reportWebVitals';
 
 
 ReactDOM.render(
+  <React.StrictMode>
     <Provider store={store}>
        <App />
-    </Provider>,
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
